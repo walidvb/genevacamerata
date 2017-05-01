e45f36f6225a9ba874b8e85ca2c1d568145808dc
Walid <walidvb@gmail.com>
2017-05-01 17:53:46 +0300

    adds date filter

--- sites/all/themes/light_skeleton/js/home_filters.js
+++ sites/all/themes/light_skeleton/js/home_filters.js
@@ -1,18 +1,44 @@
 (function($){
+	// dates
+	$(document).on('click', '.pretty-calendar-week a', function(e){
+		e.preventDefault();
+		var $this = $(e.currentTarget);
+		var href = $this.attr('href');
+		console.log("$this, e:", $this, e);
+		var date = /calendar\/(.+)/.exec(href)[1].replace(/\//g, '-');
+		loadWithFilters({
+			date: date,
+			clicked: $this,
+		});
+	});
+
+	// types
 	$(document).on('click', 'a[data-type]', function(e){
 		e.preventDefault();
 		var $this = $(e.target);
 		var id = $this.data('type');
-		var url = location.pathname + '?field_type=' + id;
+		loadWithFilters({
+			id: id,
+		});
+
+	});
+
+	function loadWithFilters(options){
+		var url = location.pathname;
+		url += options.id ? '?&field_type=' + options.id : '';
+		url += options.date ? '?&field_date=' + options.date : '';
 		var container = $('#home-concert-list').parent();
+		console.log("url:", url);
 		container.addClass('loading').load(url + ' #home-concert-list', function(){
 			container.removeClass('loading');
-			activateFilter();
-		})
-
-		function activateFilter(){
 			$('a[data-type]').removeClass('active');
-			$('a[data-type="'+id+'"]').addClass('active');
-		}
-	});
+			$('.pretty-calendar-week a').removeClass('active');
+			if(options.id){
+				$('a[data-type="'+options.id+'"]').addClass('active');
+			}
+			else if(options.date){
+				options.clicked.addClass('active');
+			}
+		});
+	};
 })(jQuery);
