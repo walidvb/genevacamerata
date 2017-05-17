e8c9b749de435021f18f562bc80d1a10a49b0c3a
Walid <walidvb@gmail.com>
2017-05-18 00:28:29 +0200

    some calendar and filters css fix

--- sites/all/themes/light_skeleton/js/myScripts.js
+++ sites/all/themes/light_skeleton/js/myScripts.js
@@ -2,12 +2,14 @@
 	$(document).ready(function(){
 	});
 
+
 	$(document).on('click', '[data-panel-target]',function(e){
 		var burger = $(e.currentTarget);
 		var targetSelector = burger.data('panel-target');
 		$('.open.is-active').not(targetSelector).not(burger).removeClass('open is-active');
 		$(targetSelector).add(burger).toggleClass('open is-active');
-		$('html').toggleClass("has-panel-open");
+		console.log("$('.is-active').length > 0:", $('.is-active').length > 0);
+		$('html').toggleClass("has-panel-open", $('.is-active').length > 0);
 	});
 
 	$(document).on('click', '.krumo-root',function(e){
