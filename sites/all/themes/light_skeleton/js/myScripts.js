6f58298cc04b30102cd19fab15f3cebff4dd0d4f
Walid <walidvb@gmail.com>
2017-05-25 13:05:41 -0500

    fixes menu

--- sites/all/themes/light_skeleton/js/myScripts.js
+++ sites/all/themes/light_skeleton/js/myScripts.js
@@ -13,8 +13,14 @@
 	$(document).ready(function(){
 		$('[data-panel-target]').on('click touch', function(e){
 			var burger = $(e.currentTarget);
-			var targetSelector = burger.data('panel-target');
-			$('html').attr('data-open', targetSelector.substring(1)).toggleClass("has-panel-open");
+			var targetSelector = burger.data('panel-target').substring(1);
+			var currentPanel = $('html').attr('data-panel');
+			if(currentPanel == targetSelector){
+				$('html').attr('data-panel', '').removeClass("has-panel-open");
+			}
+			else{
+				$('html').attr('data-panel', targetSelector).addClass("has-panel-open");	
+			}
 		});
 
 		// allow scrolling when dpm()'ing
